8bb76b2d905338974e8bcbd03b714a23bacdff19
Cole Gibbs <colegibbs0@gmail.com>
2022-03-15 13:50:17 -0700

    challenge 3

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -35,6 +35,8 @@ Write a function named addQuestion that, given an array of strings, uses map to 
 
 const addQuestion = (arr) => {
   // Solution code here...
+  let questionStr = arr.map(str => `${str}?`);
+  return questionStr;
 };
 
 /* ------------------------------------------------------------------------------------------------
