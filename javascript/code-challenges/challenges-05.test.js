b744f829c90a394b755f05ef697ee8983bdbafda
Cole Gibbs <colegibbs0@gmail.com>
2022-03-18 13:40:54 -0700

    challenge 3

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -42,6 +42,7 @@ Write a function named addPurchases that, given an array of objects as input, us
 
 const addPurchases = (arr) => {
   // Solution code here...
+  return arr.reduce((total, current) => total + current.purchasePrice, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
