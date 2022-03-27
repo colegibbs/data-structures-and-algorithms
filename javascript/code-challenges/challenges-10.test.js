8343e77982edc382afa5509f3ebacafd668da0cb
Cole Gibbs <colegibbs0@gmail.com>
2022-03-26 19:06:54 -0700

    challenge 6

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -149,6 +149,7 @@ const errands = [
 
 const howManyTreats = (arr) => {
   // Solution code here...
+  return arr[2].items[1].quantity;
 };
 
 /* ------------------------------------------------------------------------------------------------
