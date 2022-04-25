e912c6f94a50e71ed07a32e61d413a5e252dbe87
Cole Gibbs <colegibbs0@gmail.com>
2022-04-25 16:12:58 -0700

    test_append passing

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -11,16 +11,26 @@ class LinkedList:
 
 
     def __str__(self):
-        str = "NULL"
         current = self.head
+        str = ""
         while current:
-            if current.next:
-                str = f"{{ {current.value} }} -> {{ {current.next.value} }} -> " + str
-            else:
-                if str == "NULL":
-                    str = f"{{ {current.value} }} -> " + str
+            str += f"{{ {current.value} }} -> "
             current = current.next
+        str += "NULL"
         return str
+
+
+
+        # str = "NULL"
+        # current = self.head
+        # while current:
+        #     if current.next:
+        #         str = f"{{ {current.value} }} -> {{ {current.next.value} }} -> " + str
+        #     else:
+        #         if str == "NULL":
+        #             str = f"{{ {current.value} }} -> " + str
+        #     current = current.next
+        # return str
 
     def includes(self, search_value):
         current = self.head
@@ -29,6 +39,19 @@ class LinkedList:
                 return True
             current = current.next
         return False
+
+    def append(self, new_node_value):
+        '''
+        traverse through the list until the current.next === None.
+        set current.next to the new node.
+        ser the next of the new node to None.
+        '''
+        current = self.head
+        while current:
+            if current.next == None:
+                current.next = Node(new_node_value)
+                break
+            current = current.next
 
 class Node:
     def __init__(self, value, next_ = None):
