725caa49c101a51651f42a89604e00688ba130b3
Cole Gibbs <colegibbs0@gmail.com>
2022-05-01 18:53:04 -0700

    all stack tests passing

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,3 +1,6 @@
+from data_structures.linked_list import Node
+from data_structures.invalid_operation_error import InvalidOperationError
+
 class Stack:
     """
     Put docstring here
@@ -5,8 +8,27 @@ class Stack:
 
     def __init__(self):
         # initialization here
-        pass
+        self.top = None
 
-    def some_method(self):
+    def push(self, value):
         # method body here
-        pass
+        self.top = Node(value, self.top)
+
+    def pop(self):
+        if self.top == None:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        old_top = self.top
+        self.top = old_top.next
+        old_top.next = None
+        return old_top.value
+
+    def is_empty(self):
+        if self.top == None:
+            return True
+        else:
+            return False
+
+    def peek(self):
+        if self.top == None:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        return self.top.value
