5106d818e00b6dc62129b0b634033ee4c611fa38
Cole Gibbs <colegibbs0@gmail.com>
2022-05-03 20:21:28 -0700

    all tests pass

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -2,7 +2,25 @@ from data_structures.queue import Queue
 
 
 class AnimalShelter:
-    pass
+    def __init__(self):
+        self.cat_queue = Queue()
+        self.dog_queue = Queue()
+
+    def enqueue(self, animal):
+        if isinstance(animal, Cat):
+            self.cat_queue.enqueue(animal)
+        elif isinstance(animal, Dog):
+            self.dog_queue.enqueue(animal)
+
+    def dequeue(self, species):
+        if species != "cat" and species != "dog":
+            return None
+        elif species == "cat":
+            removed = self.cat_queue.dequeue()
+        elif species == "dog":
+            removed = self.dog_queue.dequeue()
+
+        return removed
 
 
 class Dog:
