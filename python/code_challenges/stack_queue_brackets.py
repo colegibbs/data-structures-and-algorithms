ff536fe992044742769e59ef5b1bfddc537666f3
Cole Gibbs <colegibbs0@gmail.com>
2022-05-04 22:24:24 -0700

    all test passing

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,30 @@
+from tabnanny import check
 from data_structures.queue import Queue
 
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(string):
+    openings = ["(", "[", "{"]
+    closings = [")", "]", "}"]
+    check_open = []
+    for char in string:
+        if char in openings:
+            check_open.append(char)
+
+        if char in closings:
+            if check_open == []:
+                return False
+            if char == closings[0] and check_open[len(check_open) - 1] == openings[0]:
+                check_open.remove(openings[0])
+                continue
+            elif char == closings[1] and check_open[len(check_open) - 1] == openings[1]:
+                check_open.remove(openings[1])
+                continue
+            elif char == closings[2] and check_open[len(check_open) - 1] == openings[2]:
+                check_open.remove(openings[2])
+                continue
+
+    return True if check_open == [] else False
+
+
+
+
