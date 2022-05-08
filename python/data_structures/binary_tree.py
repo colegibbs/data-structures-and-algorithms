5ead422cc4778e500d0f1015f1785e2587808e3f
Cole Gibbs <colegibbs0@gmail.com>
2022-05-08 14:12:47 -0700

    binary tree tests passing

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -4,13 +4,54 @@ class BinaryTree:
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.root = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def pre_order(self):
+
+        def traverse(root, node_values):
+            if root == None:
+                return
+
+            node_values.append(root.value)
+            traverse(root.left, node_values)
+            traverse(root.right, node_values)
+
+        all_values = []
+        traverse(self.root, all_values)
+
+        return all_values
+
+    def in_order(self):
+        def traverse(root, node_values):
+            if root == None:
+                return
+
+            traverse(root.left, node_values)
+            node_values.append(root.value)
+            traverse(root.right, node_values)
+
+        all_values = []
+        traverse(self.root, all_values)
+
+        return all_values
+
+    def post_order(self):
+        def traverse(root, node_values):
+            if root == None:
+                return
+
+            traverse(root.left, node_values)
+            traverse(root.right, node_values)
+            node_values.append(root.value)
+
+        all_values = []
+        traverse(self.root, all_values)
+
+        return all_values
 
 
 class Node:
-    pass
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
